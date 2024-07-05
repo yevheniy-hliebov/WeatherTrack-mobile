307eccf151cc1766d8f8a266c8eb6d0b37ede6c8
Yevhenii Hliebov <yevheniy.hliebov@gmail.com>
2024-07-06 00:34:04 +0300

    update env

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -4,9 +4,7 @@ class Config {
   static final String weatherApiKey = dotenv.get('WEATHER_API_KEY');
   static final String weatherApiUrl = dotenv.get('WEATHER_API_URL');
 
+  static final String xRapidapiKey = dotenv.get('X_RAPIDAPI_KEY');
   static final String geoApiUrl = dotenv.get('GEO_API_URL');
   static final String geoPopulation = dotenv.get('GEO_POPULATION');
-
-  static final String xRapidapiKey = dotenv.get('X_RAPIDAPI_KEY');
-  static final String xRapidapiHost = dotenv.get('X_RAPIDAPI_HOST');
 }
