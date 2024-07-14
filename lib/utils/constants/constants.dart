ea0f98a1dfa4b2754d66d74f7fd27832d275c3e8
Yevhenii Hliebov <yevheniy.hliebov@gmail.com>
2024-07-14 10:21:39 +0300

    add size constants and export all files from constants folder

---
+++ lib/utils/constants/constants.dart
@@ -0,0 +1,5 @@
+export 'api_constants.dart';
+export 'colors.dart';
+export 'images.dart';
+export 'sizes.dart';
+export 'weather_icons.dart';
---
+++ lib/utils/constants/sizes.dart
@@ -0,0 +1,40 @@
+class Sizes {
+  Sizes._();
+
+  // Padding and margin sizes
+  static const double xs = 4;
+  static const double sm = 10;
+  static const double md = 16;
+  static const double md2 = 19;
+  static const double lg = 25;
+
+  // Loader sizes
+  static const double loaderStrokeWidth = 2;
+  static const double loaderSize = 35;
+
+  // Divider sizes
+  static const double dividerHeight = 1;
+
+  // Blur values
+  static const double blurValueXs = 2;
+  static const double blurValueSm = 4;
+
+  // Container sizes
+  static const double containerRadiusSm = 10;
+  static const double containerRadiusLg = 25;
+  static const double containerBorderWidth = 1;
+
+  // Size city bar
+  static const double customSearchBarHeight = 70;
+
+  // Default spacing between items
+  static const double defaultSpace = 10;
+  static const double spaceBtwSections = 20;
+
+  // Tab buttons sizes
+  static const double tabButtonWidth = 140;
+  static const double tabButtonRadius = 50;
+
+  // Font sizes
+  static const double displayLargeSize = 72;
+}
